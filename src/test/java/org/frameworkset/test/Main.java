08928c6fd69183786c69927f5ae762c4a4994229
大河 <yin-bp@163.com>
2025-08-21 17:24:19 +0800

    升级框架版本号： bboss 6.3.0 - 6.3.1 elasticsearch datatran 7.3.9` - 7.5.0 bboss-spring-boot-starter  6.3.9 - 6.5.0 bboss-http 6.3.9 - 6.5.0 bboss-plugin 6.5.1 - 6.5.2 bboss-data  6.3.2 - 6.3.3 security  6.3.0 - 6.3.1

--- src/test/java/org/frameworkset/test/Main.java
+++ src/test/java/org/frameworkset/test/Main.java
@@ -15,9 +15,12 @@ package org.frameworkset.test;/*
  */
 
 import org.frameworkset.boot.ApplicationStart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main
 {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args){
 		ApplicationStart.main(args);
 	}
