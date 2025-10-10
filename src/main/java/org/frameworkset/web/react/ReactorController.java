8fd7053ce96792ac3e1f16feea73c9f93f8e9535
大河 <yin-bp@163.com>
2025-10-10 18:23:43 +0800

    http proxy改进：增加链接maxIdleTime空闲时间配置，  单位:秒，链接空闲时间达到maxIdleTime时，将被回收掉 http proxy5改进：增加stream处理api，支持大模型流式处理调用，并增加案例：https://gitee.com/bboss/bbootdemo/tree/master/src/main/java/org/frameworkset/web/react/ReactorController.java

--- src/main/java/org/frameworkset/web/react/ReactorController.java
+++ src/main/java/org/frameworkset/web/react/ReactorController.java
@@ -15,18 +15,24 @@ package org.frameworkset.web.react;
  * limitations under the License.
  */
 
+import org.frameworkset.spi.InitializingBean;
+import org.frameworkset.spi.remote.http.HttpRequestProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author biaoping.yin
  * @Date 2025/9/30
  */
-public class ReactorController {
+public class ReactorController implements InitializingBean {
     private Logger logger = LoggerFactory.getLogger(ReactorController.class);
     public Flux<String> chatTest() {
 
@@ -35,8 +41,8 @@ public class ReactorController {
 
         Flux<String> flux = Flux.interval(Duration.ofSeconds(1))
                 .map(i -> {
-                    String data = "data: "+ i +"," + LocalDateTime.now() + "\n\n";
-                    System.out.println(data);
+                    String data = "中文data: "+ i +"," + LocalDateTime.now() + "\n\n";
+                    logger.info(data);
                     return data;
                 })
                 .take(10); // 限制发送10条数据
@@ -45,4 +51,44 @@ public class ReactorController {
 //        return Flux.interval(Duration.ofSeconds(1)).take(10).map(sequence -> "{" + "    \"data\": \"33\"," + "    \"count\": \"" + sequence + "\"" + "}");
     }
 
+
+    public Flux<String> deepseekChat(String message) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("model", "deepseek-chat");
+
+        List<Map<String, String>> messages = new ArrayList<>();
+        Map<String, String> userMessage = new HashMap<>();
+        userMessage.put("role", "user");
+        userMessage.put("content", message);
+        messages.add(userMessage);
+
+        requestMap.put("messages", messages);
+        requestMap.put("stream", true);
+        requestMap.put("max_tokens", 2048);
+        requestMap.put("temperature", 0.7);
+        return HttpRequestProxy.streamChatCompletion("/chat/completions",requestMap);
+//                .doOnSubscribe(subscription -> logger.info("开始订阅流..."))
+//                .doOnNext(chunk -> System.out.print(chunk))
+//                .doOnComplete(() -> logger.info("\n=== 流完成 ==="))
+//                .doOnError(error -> logger.error("错误: " + error.getMessage(),error));
+//                .subscribe();
+
+    }
+
+
+    /**
+     * Invoked by a BeanFactory after it has set all bean properties supplied
+     * (and satisfied BeanFactoryAware and ApplicationContextAware).
+     * <p>This method allows the bean instance to perform initialization only
+     * possible when all bean properties have been set and to throw an
+     * exception in the event of misconfiguration.
+     *
+     * @throws Exception in the event of misconfiguration (such
+     *                   as failure to set an essential property) or if initialization fails.
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //加载配置文件，启动负载均衡器
+        HttpRequestProxy.startHttpPools("application.properties");
+    }
 }
