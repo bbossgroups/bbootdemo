4899ce1fb33886a579743e2afc0b5305453fc6fd
大河 <yin-bp@163.com>
2026-02-01 00:47:14 +0800

    增加图生视频功能：根据首帧、尾帧和prompt生成视频。

--- src/main/java/org/frameworkset/web/react/ReactorController.java
+++ src/main/java/org/frameworkset/web/react/ReactorController.java
@@ -829,17 +829,81 @@ public class ReactorController implements InitializingBean {
         if(SimpleStringUtil.isEmpty( message)){
             message = "一幅史诗级可爱的场景。一只小巧可爱的卡通小猫将军，身穿细节精致的金色盔甲，头戴一个稍大的头盔，勇敢地站在悬崖上。他骑着一匹虽小但英勇的战马，说：”青海长云暗雪山，孤城遥望玉门关。黄沙百战穿金甲，不破楼兰终不还。“。悬崖下方，一支由老鼠组成的、数量庞大、无穷无尽的军队正带着临时制作的武器向前冲锋。这是一个戏剧性的、大规模的战斗场景，灵感来自中国古代的战争史诗。远处的雪山上空，天空乌云密布。整体氛围是“可爱”与“霸气”的搞笑和史诗般的融合。";
         }
+        List<String> imagesBase64  = (List)questions.get("imagesBase64");
+        String imageUrl = (String)questions.get("imageUrl");
+        if(imageUrl != null) {
+            imageUrl = imageUrl.trim();
+        }
+
+
+
+
+        
         VideoAgentMessage videoAgentMessage = new VideoAgentMessage();
-        videoAgentMessage.setModel("wan2.6-t2v")
-                .setPrompt( message)
-                .setAudioUrl("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250923/hbiayh/%E4%BB%8E%E5%86%9B%E8%A1%8C.mp3")
-//                .setLanguageType("Chinese")
-                .addParameter("size","1280*720")
-                .addParameter("prompt_extend",true)
-                .addParameter("duration",10)
-                .addParameter("shot_type","multi");
+        String model = null;
+        if(SimpleStringUtil.isNotEmpty(imageUrl)) {
+            if(imageUrl.indexOf(",") > 0){
+                String[] imageUrls = imageUrl.split(",");
+                videoAgentMessage.setFirstFrameUrl(imageUrls[0]);
+                videoAgentMessage.setLastFrameUrl(imageUrls[1]);
+                model = "wan2.2-kf2v-flash";
+            }
+            else {
+                videoAgentMessage.setImgUrl(imageUrl);
+                videoAgentMessage.setAudioUrl("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250923/hbiayh/%E4%BB%8E%E5%86%9B%E8%A1%8C.mp3");
+                model = "wan2.6-i2v-flash";
+            }
+        }
+        else if(imagesBase64 != null && imagesBase64.size() > 0){
+            if(imagesBase64.size() > 1){
+                videoAgentMessage.setFirstFrameUrl(imagesBase64.get(0));
+                videoAgentMessage.setLastFrameUrl(imagesBase64.get(1));
+                model = "wan2.2-kf2v-flash";
+            }
+            else{
+                videoAgentMessage.setImgUrl(imagesBase64.get(0));
+                videoAgentMessage.setAudioUrl("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250923/hbiayh/%E4%BB%8E%E5%86%9B%E8%A1%8C.mp3");
+                model = "wan2.6-i2v-flash";
+            }
+           
+        }
+        else{
+            videoAgentMessage.setAudioUrl("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250923/hbiayh/%E4%BB%8E%E5%86%9B%E8%A1%8C.mp3");
+            model = "wan2.6-t2v";
+        }
+        
+       
+        if(videoAgentMessage.getImgUrl() != null){
+           
+            videoAgentMessage                     
+                    .addParameter("resolution","1080P")
+                    .addParameter("prompt_extend",true)
+                    .addParameter("duration",10)
+                    .addParameter("watermark",true)
+                    .addParameter("shot_type","multi")
+                    .setTemplate("hanfu-1");
+        }
+        else  if(videoAgentMessage.getFirstFrameUrl() != null){
+
+            videoAgentMessage
+                    .addParameter("resolution","1080P")
+                    .addParameter("prompt_extend",true)
+                    
+                   ;
+        }
+        else{
+           
+            videoAgentMessage                     
+                    .addParameter("size","1280*720")
+                    .addParameter("prompt_extend",true)
+                    .addParameter("duration",10)
+                    .addParameter("shot_type","multi");
+        }
+        videoAgentMessage.setModel(model);
+        videoAgentMessage.setPrompt( message)
+                ;
        AIAgent aiAgent = new AIAgent();
-       VideoTask videoTask = aiAgent.submitVideoTask("qwenvlplus","/api/v1/services/aigc/video-generation/video-synthesis",videoAgentMessage);
+       VideoTask videoTask = aiAgent.submitVideoTask("qwenvlplus",videoAgentMessage);
       
         return videoTask;
     }
